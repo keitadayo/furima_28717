8d49c6782417de6a24d7187d8dcff5ed046ddaae
keitadayo <69033652+keitadayo@users.noreply.github.com>
2020-08-24 12:38:46 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, :birthday, presence: true
+  validates :nickname, :birthday, presence: true
   with_options presence: true do
-    validates :email, format: { with:/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "メールアドレスには@を入力してください"}
-    validates :password, format: { with:/.{6,}/, message: "パスワードは6文字以上入力してください。"}
-    validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "パスワードは英数字混合で入力してください。"}
-    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "お名前(全角)は全角で入力してください。"}
-    validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "お名前カナ(全角)は全角カタカナで入力して下さい。"}
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'メールアドレスには@を入力してください' }
+    validates :password, format: { with: /.{6,}/, message: 'パスワードは6文字以上入力してください。' }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'パスワードは英数字混合で入力してください。' }
+    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'お名前(全角)は全角で入力してください。' }
+    validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'お名前カナ(全角)は全角カタカナで入力して下さい。' }
   end
-
 end
