086bc819012d8e014731e4bfb07f5c2e896c538a
keitadayo <69033652+keitadayo@users.noreply.github.com>
2020-08-28 22:57:06 +0900

    購入画面の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :move_to_user_login
+
+  def index
+    @order = Order.new
+    @item = Item.find(params[:item_id])
+    @shipping_address = ShippingAddress.new
+  end
+
+  def move_to_user_login
+    redirect_to new_user_session_path unless user_signed_in?
+  end
 end
